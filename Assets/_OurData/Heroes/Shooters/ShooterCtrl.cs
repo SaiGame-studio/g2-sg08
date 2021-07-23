7e2375270e91c57765b939d3c26df0d5b9682785
SimonHome <tranminhduc18116@gmail.com>
2021-07-23 22:45:44 +0700

    e-5b

--- Assets/_OurData/Heroes/Shooters/ShooterCtrl.cs
+++
@@ -1,9 +0,0 @@
-using Assets.HeroEditor.Common.CharacterScripts;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ShooterCtrl : HeroCtrl
-{
-    //[Header("Shooter")]
-}
