15296da301d1e51d91feff18f4ec09ebec6124fa
SimonHome <tranminhduc18116@gmail.com>
2021-08-02 22:49:08 +0700

    E-11b

--- Assets/_OurData/Heroes/HeroProfile.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName ="Hero Profile",menuName ="Hero/Create Profile")]
-public class HeroProfile : ScriptableObject
-{
-    public string heroClass;   
-}
