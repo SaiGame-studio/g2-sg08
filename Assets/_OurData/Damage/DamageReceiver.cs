d908279d0d7ae233742248a9f918ba511d6bfe2c
SimonHome <tranminhduc18116@gmail.com>
2021-08-19 23:04:46 +0700

    E-21

--- Assets/_OurData/Damage/DamageReceiver.cs
+++ Assets/_OurData/Damage/DamageReceiver.cs
@@ -6,6 +6,7 @@ public class DamageReceiver : SaiBehaviour
 {
     [Header("DamageReceiver")]
     [SerializeField] protected int hp = 1;
+    [SerializeField] protected string deadEffect = "EnemyDeath1";
 
     public virtual bool IsDead()
     {
@@ -15,5 +16,28 @@ public class DamageReceiver : SaiBehaviour
     public virtual void Receive(int damage)
     {
         this.hp -= damage;
+
+        this.Dying();
+    }
+
+    protected virtual void Dying()
+    {
+        if (!this.IsDead()) return;
+
+        this.ShowDeadEffect();
+        this.Despawn();
+    }
+
+    protected virtual void ShowDeadEffect()
+    {
+        Transform effect = ObjPoolManager.instance.Spawn(this.deadEffect, transform.position, transform.rotation);
+        effect.gameObject.SetActive(true);
+    }
+
+    protected virtual void Despawn()
+    {
+        bool isAlreadyPool = ObjPoolManager.instance.Pool().GetPrefabPool(transform) == null ? false : true;
+        if (isAlreadyPool) ObjPoolManager.instance.Despawn(transform);
+        else Destroy(gameObject);
     }
 }
