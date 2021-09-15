f90ded98d2cf980fd49e04226637f046e5ff07f5
SimonHome <tranminhduc18116@gmail.com>
2021-09-15 19:44:52 +0700

    e-39a

---
+++ Assets/_OurData/Manager/GameLevelManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class GameLevelManager : MonoBehaviour
+{
+    public static GameLevelManager instance;
+
+    [Header("Game Level")]
+    [SerializeField] protected int level = 0;
+    [SerializeField] protected float secondPerLevel = 30;
+    [SerializeField] protected float timer = 0;
+
+    private void Awake()
+    {
+        if (GameLevelManager.instance != null) Debug.LogError("Only 1 GameLevelManager allow");
+        GameLevelManager.instance = this;
+    }
+
+    private void FixedUpdate()
+    {
+        this.timer += Time.fixedDeltaTime;
+        this.LevelCalculate();
+    }
+
+    protected void LevelCalculate()
+    {
+        this.level = (int) Mathf.Floor(this.timer / this.secondPerLevel);
+        this.level += 1;
+    }
+}
