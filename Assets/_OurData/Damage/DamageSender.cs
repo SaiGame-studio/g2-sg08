f3ae742e4acec049186cdd72702b23e6bee74f3c
SimonHome <tranminhduc18116@gmail.com>
2021-09-22 02:14:45 +0700

    E-44

--- Assets/_OurData/Damage/DamageSender.cs
+++ Assets/_OurData/Damage/DamageSender.cs
@@ -11,23 +11,7 @@ public class DamageSender : SaiBehaviour
     {
         //Debug.Log("this: " + transform.name);
         //Debug.Log("collision: " + collision.gameObject.name);
-        this.SendDamage(collision);
-        //this.SendDamage(collision.gameObject);
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        //Debug.Log("this: " + transform.name);
-        //Debug.Log("Collider: " + other.gameObject.name);
-        this.SendDamage(other.gameObject);
-    }
-
-    protected virtual void SendDamage(Collision collision)
-    {
-        DamageReceiver damageReveiver = collision.gameObject.GetComponent<DamageReceiver>();
-        if (damageReveiver == null) return;
-
-        damageReveiver.Receive(this.damage);
+        this.SendDamage(collision.gameObject);
     }
 
     protected virtual void SendDamage(GameObject coliderObj)
